188147306259d9834654024feb7e02ecca930806
Waqardev <Waqardev@users.noreply.github.com>
2020-01-01 17:24:28 +0500

    Connecting To MongoDB With Mongoose

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//Connect To MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDb Connected"))
+  .catch(err => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello World"));
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));

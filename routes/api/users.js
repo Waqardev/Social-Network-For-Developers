91737cb10a18979c52549a53c920491756f331f1
Waqardev <Waqardev@users.noreply.github.com>
2020-01-10 17:05:36 +0500

    Email  Password Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -44,4 +44,28 @@ router.post("/register", (req, res) => {
   });
 });
 
+//@route GET api/users/login
+//@desc  Login User / Returning JWT Token
+//@acess Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({ email }).then(user => {
+    //Check for user
+    if (!user) {
+      return res.status(404).json({ email: "user not found" });
+    }
+    //Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "Password Incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;

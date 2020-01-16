31e24fe0c31f8034c8734f3a8883598be3cfcfbd
Waqardev <Waqardev@users.noreply.github.com>
2020-01-16 17:51:19 +0500

    Create Post Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Load the Post model
+const Post = require("../../models/Post");
+
+//Load Validation
+const validatePostInput = require("../../validation/post");
 
 //@route GET api/posts/test
 //@desc  Test post route
 //@acess Public
 router.get("/test", (req, res) => res.json({ msg: "posts works" }));
+
+// @route   POST api/posts
+// @desc    Create Post
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //Check Validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;

53def2174f77c7706da26246bb6bad19de9d25c7
Waqardev <Waqardev@users.noreply.github.com>
2020-01-20 14:32:01 +0500

    React Router Setup (v4)

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import register from "./components/auth/register";
+import login from "./components/auth/login";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={register} />
+          <Route exact path="/login" component={login} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 

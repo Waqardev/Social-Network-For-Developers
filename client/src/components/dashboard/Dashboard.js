031ef456c9de1c87a75f72c958df75ebdc422b6e
Waqardev <Waqardev@users.noreply.github.com>
2020-02-04 16:59:30 +0500

    Dashboard Experience Display  Delete

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { getCurrentProfile, deleteAccount } from "../../actions/profileActions";
 import Spinner from "../common/Spinner";
 import ProfileAction from "./ProfileAction";
+import Experience from "./Experience";
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -30,7 +31,7 @@ class Dashboard extends Component {
               Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
             </p>
             <ProfileAction />
-            {/* TODO: exp and edu */}
+            <Experience experience={profile.experience} />
             <div style={{ marginBottom: "60px" }}>
               <button
                 onClick={this.onDeleteClick.bind(this)}

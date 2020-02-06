4c3716d92ea31ab02ef1ff32299d3f1647fe8996
Waqardev <Waqardev@users.noreply.github.com>
2020-02-06 18:29:42 +0500

    Profile Header

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { getProfileByHandle } from "../../actions/profileActions";
+import ProfileHeader from "./ProfileHeader";
+import Spinner from "../common/Spinner";
 
 class Profile extends Component {
   componentDidMount() {
@@ -10,9 +13,33 @@ class Profile extends Component {
     }
   }
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back To Profiles
+              </Link>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <ProfileHeader profile={profile} />
+        </div>
+      );
+    }
     return (
-      <div>
-        <h1>TODO:Profile</h1>
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
       </div>
     );
   }

53625a3747e52c758743a5425355c1e783eb983f
Waqardev <Waqardev@users.noreply.github.com>
2020-02-10 18:50:37 +0500

    Post State  addPost Action

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import testReducer from "../test/reducers/testReducer";
 import profileReducer from "./profileReducer";
+import postReducer from "./postReducer";
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
   test: testReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 });

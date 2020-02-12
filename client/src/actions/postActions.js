5eab880faabfea05c313a7478120e6acfa5c2412
Waqardev <Waqardev@users.noreply.github.com>
2020-02-12 18:56:54 +0500

    Single Post Display

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,6 +3,7 @@ import {
   ADD_POST,
   GET_ERRORS,
   GET_POSTS,
+  GET_POST,
   POST_LOADING,
   DELETE_POST
 } from "./types";
@@ -44,6 +45,25 @@ export const getPosts = () => dispatch => {
     );
 };
 
+//Get post
+export const getPost = id => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POST,
+
+        payload: null
+      })
+    );
+};
 //Delete post
 export const deletePost = id => dispatch => {
   axios

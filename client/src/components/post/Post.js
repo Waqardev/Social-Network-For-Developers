afb922a5706291dead7dfd0fc7c4d6a53aabc084
Waqardev <Waqardev@users.noreply.github.com>
2020-02-13 15:19:21 +0500

    Comment Form Component  Action

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Spinner from "../common/Spinner";
 import PostItem from "../posts/PostItem";
+import CommentForm from "./CommentForm";
 import { getPost } from "../../actions/postActions";
 
 class Post extends Component {
@@ -16,7 +17,12 @@ class Post extends Component {
     if (post === null || loading || Object.keys(post).length === 0) {
       postContent = <Spinner />;
     } else {
-      postContent = <PostItem post={post} showActions={false} />;
+      postContent = (
+        <div>
+          <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
+        </div>
+      );
     }
     return (
       <div className="post">
